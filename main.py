219a00bab6dccbe3b9eaf89d89489fab363afdf9
chenqi <qichen0316@gmail.com>
2022-12-22 12:15:51 +0800

    add direct concat

--- main.py
+++ main.py
@@ -16,8 +16,9 @@ parser.add_argument('-s', '--source', type=str, required=True, help='The path of
 parser.add_argument('-t', '--target', type=str, required=True, help='The path of target image.')
 parser.add_argument('-p', '--pattern', type=str, default=None, help='The path of mask pattern, if = None, don\'t change pattern.')
 parser.add_argument('-r', '--resize', type=float, default=1.0, help='resize the target image with this ratio.')
-parser.add_argument('--gaussian_kernel', type=int, default=None, help='The size of gaussian kernel.')
+parser.add_argument('-g', '--gaussian_kernel', type=int, default=None, help='The size of gaussian kernel.')
 parser.add_argument('-a', '--automatic', action='store_true', help='Use MODnet for automatic matting.')
+parser.add_argument('-d', '--direct', action='store_true', help='Use direct concat instead of poisson image editing.')
 
 args = parser.parse_args()
 
@@ -54,6 +55,7 @@ if __name__ == '__main__':
         mp = MaskPainter(path.dirname(args.source), source)
         target_mask = mp.paint_mask() 
         target_mask = target_mask.astype(np.uint8)   
+    cv2.imwrite('matting.jpg', target_mask)
     foreground = (target_mask/255*source).astype(np.uint8)
 
     if args.pattern:
@@ -65,6 +67,7 @@ if __name__ == '__main__':
             mp = MaskPainter(path.dirname(args.source), source)
             pattern_mask = mp.paint_mask() 
             # resize target and source
+        cv2.imwrite('mask.jpg', pattern_mask)
         if args.resize != 1.0:
             target = cv2.resize(target, None, fx=args.resize, fy=args.resize, interpolation=cv2.INTER_AREA)
         if source.shape[0] > target.shape[0] or source.shape[1] > target.shape[1]:
@@ -116,7 +119,12 @@ if __name__ == '__main__':
     ## as affine transformation is already performed on source, the offset should be set to 0 to avoid repeated transformation
     offset = 0, 0
 
-    poisson_blend_result = poisson_edit(source, target, target_mask, offset, args.gaussian_kernel)
+    if args.direct:
+        target_mask = target_mask[:, :, np.newaxis]
+        target_mask = np.concatenate([target_mask, target_mask, target_mask], axis=2)
+        poisson_blend_result = np.where(target_mask == 0, target, source)
+    else:
+        poisson_blend_result = poisson_edit(source, target, target_mask, offset, args.gaussian_kernel)
     
     
     print(f'source -> target: source.shape={source.shape}, target_mask.shape={target_mask.shape}, poisson_blend_result.shape={poisson_blend_result.shape}')
